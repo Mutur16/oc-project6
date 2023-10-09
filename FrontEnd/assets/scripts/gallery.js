03c92ccf9beb9901e224aa6f439c1ad1ac05e3b3
Mutur <komorebi.amm@gmail.com>
2023-10-09 14:22:26 +0200

    gallery + buttons fixes

--- FrontEnd/assets/scripts/gallery.js
+++ FrontEnd/assets/scripts/gallery.js
@@ -1,34 +1,83 @@
-updateWorks()
+const apiRoute = "http://localhost:5678/api/";
+let worksData = [];
+let workIdsSet = new Set(); 
 
-async function updateWorks(categoryId) {
-    document.querySelector(".gallery").innerHTML = ''
+async function getWorks(categoryId) {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = "";
 
-    const reponse = await fetch(`http://localhost:5678/api/works`);
-    const works = await reponse.json();
-    const gallery = document.querySelector(".gallery");
+  if (!workIdsSet.size) {
+    try {
+      const response = await fetch(`${apiRoute}works`);
+      const works = await response.json();
 
-    for (let i = 0; i < works.length; i++) {
-
-        if (categoryId === works[i].categoryId || categoryId === undefined) {
-
-            const workElement = document.createElement("figure");
-            const imageElement = document.createElement("img")
-            const titleElement = document.createElement("figcaption");
-
-            imageElement.setAttribute("src", `${works[i].imageUrl}`);
-            imageElement.setAttribute("alt", `${works[i].title}`);
-            titleElement.innerHTML = `${works[i].title}`;
-
-            gallery.appendChild(workElement);
-            workElement.appendChild(imageElement);
-            workElement.appendChild(titleElement);
+      works.forEach((work) => {
+        if (!workIdsSet.has(work.id)) {
+          workIdsSet.add(work.id);
+          worksData.push(work);
         }
+      });
+    } catch (error) {
+      console.error("Error fetching works:", error);
+      return;
     }
-    document.querySelector('.btn.active').classList.remove("active")
-    document.querySelector(`.btn[data-category="${undefined === categoryId ? "" : categoryId}"]`).classList.add("active")
+  }
+
+  const filteredWorks = !categoryId ? worksData : worksData.filter((work) => work.categoryId === categoryId);
+
+  filteredWorks.forEach((work) => {
+    const workElement = document.createElement("figure");
+    const img = document.createElement("img");
+    const title = document.createElement("figcaption");
+
+    img.src = work.imageUrl;
+    img.alt = work.title;
+    title.textContent = work.title;
+
+    workElement.appendChild(img);
+    workElement.appendChild(title);
+
+    gallery.appendChild(workElement);
+  });
 }
 
-document.querySelectorAll('.btn').forEach(button => button.addEventListener("click", (event) => {
-    const targetElement = event.target
-    updateWorks(targetElement.dataset.category === "" ? undefined : parseInt(targetElement.dataset.category))
-}))
+async function createCategoryButtons() {
+  try {
+    const response = await fetch(`${apiRoute}categories`);
+    const categories = await response.json();
+    const buttonsContainer = document.querySelector(".category-buttons");
+
+    const allCategoriesButton = document.createElement("button");
+    allCategoriesButton.textContent = "Tous";
+    allCategoriesButton.classList.add("btn", "active");
+    allCategoriesButton.addEventListener("click", () => {
+      const activeButton = document.querySelector(".btn.active");
+      if (activeButton) {
+        activeButton.classList.remove("active");
+      }
+      allCategoriesButton.classList.add("active");
+      getWorks();
+    });
+    buttonsContainer.appendChild(allCategoriesButton);
+
+    categories.forEach((category) => {
+      const button = document.createElement("button");
+      button.textContent = category.name;
+      button.classList.add("btn");
+      button.addEventListener("click", () => {
+        const activeButton = document.querySelector(".btn.active");
+        if (activeButton) {
+          activeButton.classList.remove("active");
+        }
+        button.classList.add("active");
+        getWorks(category.id);
+      });
+      buttonsContainer.appendChild(button);
+    });
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+  }
+}
+
+createCategoryButtons();
+getWorks();
