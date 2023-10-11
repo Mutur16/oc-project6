aaad39da6f1054cb7a4dfa76c51942a70586ad14
Mutur <komorebi.amm@gmail.com>
2023-10-11 16:23:02 +0200

    form + login

---
+++ FrontEnd/assets/scripts/login.js
@@ -0,0 +1,37 @@
+const token = localStorage.getItem("loginData");
+
+if (token) {
+    const logged = document.querySelector(".logged");
+    const editButton = createButton("fa-regular fa-pen-to-square fa-lg", "Modifier");
+    logged.appendChild(editButton);
+
+    const body = document.querySelector("body");
+    const mainContainer = document.querySelector(".main-container");
+    const editBanner = createBanner("fa-regular fa-pen-to-square fa-lg", "Mode édition");
+    body.insertBefore(editBanner, mainContainer);
+
+    const logout = document.querySelector(".logout");
+    configureLogoutLink(logout);
+}
+
+function createButton(iconClass, text) {
+    const button = document.createElement("button");
+    button.innerHTML = `<i class="${iconClass}"></i> ${text}`;
+    return button;
+}
+
+function createBanner(iconClass, text) {
+    const editBanner = document.createElement("div");
+    editBanner.classList.add("edit-banner");
+    editBanner.innerHTML = `<i class="${iconClass}"></i> ${text}`;
+    return editBanner;
+}
+
+function configureLogoutLink(link) {
+    link.innerHTML = "logout";
+    link.href = "index.html";
+
+    link.addEventListener("click", function () {
+        localStorage.removeItem("loginData");
+    });
+}
